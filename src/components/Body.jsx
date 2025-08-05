44d4713a0911156188a21b4e06276f51f041bf21
ramesh-perabattula <rameshperabathulla23@gmail.com>
2025-08-05 20:12:07 +0530

    added render api url

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,43 +1,49 @@
-import React, { useEffect } from 'react'
-import NavBar from './NavBar'
-import { Outlet, useNavigate } from 'react-router-dom'
-import Footer from './Footer'
-import axios from 'axios'
-import { BASE_URL } from '../utils/constants'
-import { useDispatch, useSelector } from 'react-redux'
-import { addUser } from '../utils/userSlice'
+import React, { useEffect } from 'react';
+import NavBar from './NavBar';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
+import Footer from './Footer';
+import axios from 'axios';
+import { BASE_URL } from '../utils/constants';
+import { useDispatch, useSelector } from 'react-redux';
+import { addUser } from '../utils/userSlice';
 
 const Body = () => {
-  const dispatch=useDispatch();
-  const navigate=useNavigate();
-  const userData=useSelector((store)=>store.user);
-  const fetchUser=async()=>{
-    //if(!userData) return ;
-    try{
-    const result=await axios.get(BASE_URL+"/profile/view",{withCredentials:true})
-    dispatch(addUser(result.data));
-    }catch(err){
-      if(err.status===401){
-        dispatch(addUser(null));
-        navigate("/login");
-      }
-      }
-  }
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const userData = useSelector((store) => store.user);
 
-  useEffect(()=>{
-    if(!userData){
+  const fetchUser = async () => {
+    try {
+      const result = await axios.get(`${BASE_URL}/profile/view`, {
+        withCredentials: true,
+      });
+      dispatch(addUser(result.data));
+    } catch (err) {
+      if (err.response && err.response.status === 401) {
+        dispatch(addUser(null));
+         if (!["/login", "/signup"].includes(location.pathname)) {
+          navigate("/login");
+        }
+      } else {
+        console.error("Fetch user error:", err);
+      }
+    }
+  };
+
+ useEffect(() => {
+    if (!userData && !["/login", "/signup"].includes(location.pathname)) {
       fetchUser();
     }
-   },[]);
+}, [location.pathname]);
 
   return (
     <>
-        <NavBar></NavBar>
-        <Outlet></Outlet>
-        <Footer></Footer>
+      <NavBar />
+      <Outlet />
+      <Footer />
     </>
-   )
+  );
+};
 
-}
-
-export default Body
+export default Body;
