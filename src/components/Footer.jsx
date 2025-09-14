7208be5fa801b8f14edc978833a35cc0943ab712
ramesh-perabattula <rameshperabathulla23@gmail.com>
2025-09-14 11:27:13 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
 <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4 fixed bottom-0">
   <aside>
-    <p>Copyright © {new Date().getFullYear()} - All right reserved by rameshperabattula@dev</p>
+    <p>Copyright © {new Date().getFullYear()} - All right reserved by Sasidhar-Gandham@dev</p>
   </aside>
 </footer>  
 )
